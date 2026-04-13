78657bf9e7f7f33726fba0bfc2dfbae529ed3a16
Tommy Vega <tommysvs@hotmail.com>
2026-04-13 14:57:48 -0600

    adjust base URL handling

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: process.env.DEPLOY_BASE_URL ?? '/' ,
   plugins: [
-    tailwindcss(), 
-    react()
+    react(),
+    tailwindcss()    
   ],
 })
